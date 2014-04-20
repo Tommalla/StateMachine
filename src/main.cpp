952baf2f138b839211439593735c104d8b0f8831
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-04-20 14:42:37 +0200

    Added a demo.

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,52 @@
 /* Tomasz [Tommalla] Zakrzewski, 2014
  */
 #include <cstdio>
-#include <unordered_map>
+#include <ctime>
 
 #include "EnumHash.hpp"
+#include "StateMachine.hpp"
 
 using namespace std;
 
 enum class State {
-	A = 10,
-	B,
-	C = 0
+	STANDING,
+	WALKING,
+	JUMPING,
+	DIVING
+};
+
+enum class Event {
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	GAME_EVENT
 };
 
 int main() {
-	unordered_map<State, string, EnumHash<State>> m;
+	srand(time(NULL));
+	State states[] = {State::STANDING, State::WALKING, State::JUMPING, State::DIVING};
+	const char* stateNames[] = {"standing", "walking", "jumping", "diving"};
+	Event events[] = {Event::UP, Event::DOWN, Event::LEFT, Event::RIGHT, Event::GAME_EVENT};
+	const char* eventNames[] = {"UP", "DOWN", "LEFT", "RIGHT", "GAME_EVENT"};
+	//transitions
+	State from[] = {State::STANDING, State::STANDING, State::STANDING, State::WALKING, State::WALKING, State::JUMPING, State::JUMPING, State::DIVING};
+	State to[] = {State::WALKING, State::WALKING, State::JUMPING, State::JUMPING, State::STANDING, State::DIVING, State::STANDING, State::STANDING};
+	Event transitions[] = {Event::RIGHT, Event::LEFT, Event::UP, Event::UP, Event::GAME_EVENT, Event::DOWN, Event::GAME_EVENT, Event::GAME_EVENT};
+	StateMachine<State, Event> s;
+	for (int i = 0; i < 5; ++i)
+		s.addState(states[i], [&,i](){printf("Changed state to %s\n", stateNames[i]);});
 
-	m[State::A] = "A";
-	m[State::B] = "B";
-	m[State::C] = "C";
+	for (int i = 0; i < 8; ++i)
+		s.addTransition(from[i], to[i], transitions[i]);
 
-	printf("%s %s %s\n", m[State::A].c_str(), m[State::B].c_str(), m[State::C].c_str());
+	s.start(State::STANDING);
 
-	unordered_map<string, string, EnumHash<string>> m2;
+	for (int i = 0; i < 20; ++i) {
+		int id = rand() % 5;
+		printf("Event: %s\n", eventNames[id]);
+		s.trigger(events[id]);
+	}
 
-	m2["bar"] = "foo";
-
-	printf("bar: %s, asd: %s\n", m2["bar"].c_str(), m2["asd"].c_str());
+	return 0;
 }
