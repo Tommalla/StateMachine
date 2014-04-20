e99e42a6764b73d529b1e970201e692cfe91c60b
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-04-20 14:42:07 +0200

    Added performing enterAction on start and refactored code.

--- src/StateMachine.hpp
+++ src/StateMachine.hpp
@@ -5,12 +5,12 @@
 #define STATE_MACHINE_HPP
 #include <functional>
 #include <unordered_map>
-#include <unordered_set>
 
 #include "EnumHash.hpp"
 
 template<class State, class Transition>
 class StateMachine {
+	typedef std::function<void(void)> Action;
 public:
 	StateMachine();
 
@@ -25,16 +25,15 @@ public:
 	 */
 	State getCurrentState() const;
 
-	void addState(const State& state, const std::function<void(void)>& enterAction = nullptr,
-		      const std::function<void(void)>& exitAction = nullptr);
+	void addState(const State& state, const Action& enterAction = nullptr, const Action& exitAction = nullptr);
 	/**
 	 * @brief Adds a transition. Note that this implementation represents a deterministic automata - if a transition
 	 * from ``from`` using ``transition`` already exists and it leads to a state other than ``to`` it will be lost.
 	 */
 	void addTransition(const State& from, const State& to, const Transition& transition);
 
-	void setEnterAction(const State& state, const std::function<void(void)>& enterAction);
-	void setExitAction(const State& state, const std::function<void(void)>& exitAction);
+	void setEnterAction(const State& state, const Action& enterAction);
+	void setExitAction(const State& state, const Action& exitAction);
 
 	/**
 	 * @brief Starts the state machine from initialState
@@ -42,14 +41,17 @@ public:
 	void start(const State& initialState);
 
 private:
+	void executeActionsForTransition(const State& from, const State& to) const;
+	void executeAction(const Action& f) const;
+
 	typedef std::unordered_map<Transition, State, EnumHash<Transition>> Node;
 
 	bool started;
 	State currentState;
 	std::unordered_map<State, Node, EnumHash<State>> graph;
-	std::unordered_map<State, std::function<void(void)>,
+	std::unordered_map<State, Action,
 		EnumHash<State>> enterActions;
-	std::unordered_map<State, std::function<void(void)>,
+	std::unordered_map<State, Action,
 		EnumHash<State>> exitActions;
 };
 
@@ -66,10 +68,7 @@ bool StateMachine<State, Transition>::trigger(const Transition& transition) {
 	auto iter = m.find(transition);
 
 	if (iter != m.end()) {
-		auto fns[] = {exitActions[currentState], enterActions[iter->second]};
-		for (auto& f: fns)
-			if (f != nullptr)
-				f();
+		executeActionsForTransition(currentState, iter->second);
 		currentState = iter->second;
 		return true;
 	}
@@ -83,8 +82,7 @@ State StateMachine<State, Transition>::getCurrentState() const {
 }
 
 template<class State, class Transition>
-void StateMachine<State, Transition>::addState(const State& state, const std::function<void(void)>& enterAction,
-					       const std::function<void(void)>& exitAction) {
+void StateMachine<State, Transition>::addState(const State& state, const Action& enterAction, const Action& exitAction) {
 	auto iter = graph.find(state);
 	if (iter == graph.end()) {
 		graph.emplace_hint(iter, state, Node());
@@ -102,19 +100,36 @@ void StateMachine<State, Transition>::addTransition(const State& from, const Sta
 }
 
 template<class State, class Transition>
-void StateMachine<State, Transition>::setEnterAction(const State& state, const std::function<void(void)>& enterAction) {
+void StateMachine<State, Transition>::setEnterAction(const State& state, const Action& enterAction) {
 	enterActions[state] = enterAction;
 }
 
 template<class State, class Transition>
-void StateMachine<State, Transition>::setExitAction(const State& state, const std::function<void(void)>& exitAction) {
+void StateMachine<State, Transition>::setExitAction(const State& state, const Action& exitAction) {
 	exitActions[state] = exitAction;
 }
 
 template<class State, class Transition>
 void StateMachine<State, Transition>::start(const State& initialState) {
+	if (started)
+		executeActionsForTransition(currentState, initialState);
+	else
+		executeAction(enterActions[initialState]);
 	currentState = initialState;
 	started = true;
+}
+
+template<class State, class Transition>
+void StateMachine<State, Transition>::executeActionsForTransition(const State& from, const State& to) const {
+	std::function<void(void)> fns[] = {exitActions.at(from), enterActions.at(to)};
+	for (const auto& f: fns)
+		executeAction(f);
+}
+
+template<class State, class Transition>
+void StateMachine<State, Transition>::executeAction(const Action& f) const {
+	if (f != nullptr)
+		f();
 }
 
 #endif // STATE_MACHINE_HPP
